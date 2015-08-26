fc2171978a2b8331f0415aff5d64016545f5eab8
kogrego <kogrego@gmail.com>
2015-08-27 01:57:04 +0300

    varaibles names

--- MovieService/MovieServiceTest/MovieServiceTest.cs
+++ MovieService/MovieServiceTest/MovieServiceTest.cs
@@ -11,127 +11,127 @@ namespace MovieServiceTest
         [TestMethod]
         public void OmdbSearchMovieByTitleTest()
         {
-            OmdbService service = OmdbService.Service;
+            OmdbService Service = OmdbService.Service;
             title = "Fight Club";
-            SearchResult expected = new SearchResult();
-            SearchResult actual = service.SearchMovie(title);
-            Assert.IsTrue(actual.Titles.Contains("Fight Club") && actual.Years.Contains("1999"));
+            SearchResult Expected = new SearchResult();
+            SearchResult Actual = Service.SearchMovie(title);
+            Assert.IsTrue(Actual.Titles.Contains("Fight Club") && Actual.Years.Contains("1999"));
         }
 
         [TestMethod]
         [ExpectedException(typeof(TitleNotFoundException))]
         public void OmdbTitleNotFoundExceptionTest()
         {
-            OmdbService service = OmdbService.Service;
+            OmdbService Service = OmdbService.Service;
             title = "thisIsNotAnameOfAmovie";
-            service.SearchMovie(title);
+            Service.SearchMovie(title);
         }
 
         [TestMethod]
         public void OmdbGetMovieInfoTest()
         {
-            OmdbService service = OmdbService.Service;
+            OmdbService Service = OmdbService.Service;
             title = "The matrix";
-            MovieInfo expected = new MovieInfo();
-            expected.Title = "The Matrix";
-            expected.Year = "1999";
-            expected.Rated = "R";
-            expected.Released = "31 Mar 1999";
-            expected.RunTime = "136 min";
-            expected.Genre = "Action, Sci-Fi";
-            expected.Director = "Andy Wachowski, Lana Wachowski";
-            expected.Writer = "Andy Wachowski, Lana Wachowski";
-            expected.Actors = "Keanu Reeves, Laurence Fishburne, Carrie-Anne Moss, Hugo Weaving";
-            expected.Plot = "A computer hacker learns from mysterious rebels about the true nature of his reality and his role in the war against its controllers.";
-            expected.Language = "English";
-            expected.Country = "USA, Australia";
-            expected.Awards = "Won 4 Oscars. Another 33 wins & 40 nominations.";
-            expected.Rating = "8.7";
-            MovieInfo actual = service.GetMovieInfo(title);
-            Assert.AreEqual(expected, actual);
+            MovieInfo Expected = new MovieInfo();
+            Expected.Title = "The Matrix";
+            Expected.Year = "1999";
+            Expected.Rated = "R";
+            Expected.Released = "31 Mar 1999";
+            Expected.RunTime = "136 min";
+            Expected.Genre = "Action, Sci-Fi";
+            Expected.Director = "Andy Wachowski, Lana Wachowski";
+            Expected.Writer = "Andy Wachowski, Lana Wachowski";
+            Expected.Actors = "Keanu Reeves, Laurence Fishburne, Carrie-Anne Moss, Hugo Weaving";
+            Expected.Plot = "A computer hacker learns from mysterious rebels about the true nature of his reality and his role in the war against its controllers.";
+            Expected.Language = "English";
+            Expected.Country = "USA, Australia";
+            Expected.Awards = "Won 4 Oscars. Another 33 wins & 40 nominations.";
+            Expected.Rating = "8.7";
+            MovieInfo Actual = Service.GetMovieInfo(title);
+            Assert.AreEqual(Expected, Actual);
         }
 
         [TestMethod]
         [ExpectedException(typeof(TitleNotFoundException))]
         public void OmdbMovieInfoNotFoundExceptionTest()
         {
-            OmdbService service = OmdbService.Service;
+            OmdbService Service = OmdbService.Service;
             title = "thisIsNotAnameOfAmovie";
-            service.GetMovieInfo(title);
+            Service.GetMovieInfo(title);
         }
 
         [TestMethod]
         public void TmdbSearchMovieByTitleTest()
         {
-            TmdbService service = TmdbService.Service;
+            TmdbService Service = TmdbService.Service;
             title = "The Matrix";
-            SearchResult expected = new SearchResult();
-            SearchResult actual = service.SearchMovie(title);
-            Assert.IsTrue(actual.Titles.Contains("The Matrix") && actual.Years.Contains("1999-03-30"));
+            SearchResult Expected = new SearchResult();
+            SearchResult Actual = Service.SearchMovie(title);
+            Assert.IsTrue(Actual.Titles.Contains("The Matrix") && Actual.Years.Contains("1999-03-30"));
         }
 
         [TestMethod]
         [ExpectedException(typeof(TitleNotFoundException))]
         public void TmdbTitleNotFoundExceptionTest()
         {
-            TmdbService service = TmdbService.Service;
+            TmdbService Service = TmdbService.Service;
             title = "thisIsNotAnameOfAmovie";
-            service.SearchMovie(title);
+            Service.SearchMovie(title);
         }
 
         [TestMethod]
         public void TmdbGetMovieInfoTest()
         {
-            TmdbService service = TmdbService.Service;
+            TmdbService Service = TmdbService.Service;
             title = "Batman Begins";
-            MovieInfo expected = new MovieInfo();
-            expected.Actors = "Missing data";
-            expected.Awards = "Missing data";
-            expected.Country = "Missing data";
-            expected.Director = "Missing data";
-            expected.Genre = " Action, Crime, Drama, ";
-            expected.Language = "en";
-            expected.Plot = "Driven by tragedy, billionaire Bruce Wayne dedicates his life to uncovering and defeating the corruption that plagues his home, Gotham City.  Unable to work within the system, he instead creates a new identity, a symbol of fear for the criminal underworld - The Batman.";
-            expected.Rated = "Missing data";
-            expected.Rating = "Missing data";
-            expected.Released = "2005-06-15";
-            expected.RunTime = "140";
-            expected.Title = "Batman Begins";
-            expected.Writer = "Missing data";
-            expected.Year = "Missing data";
-            MovieInfo actual = service.GetMovieInfo(title);
-            Assert.AreEqual(expected, actual);
+            MovieInfo Expected = new MovieInfo();
+            Expected.Actors = "Missing data";
+            Expected.Awards = "Missing data";
+            Expected.Country = "Missing data";
+            Expected.Director = "Missing data";
+            Expected.Genre = " Action, Crime, Drama, ";
+            Expected.Language = "en";
+            Expected.Plot = "Driven by tragedy, billionaire Bruce Wayne dedicates his life to uncovering and defeating the corruption that plagues his home, Gotham City.  Unable to work within the system, he instead creates a new identity, a symbol of fear for the criminal underworld - The Batman.";
+            Expected.Rated = "Missing data";
+            Expected.Rating = "Missing data";
+            Expected.Released = "2005-06-15";
+            Expected.RunTime = "140";
+            Expected.Title = "Batman Begins";
+            Expected.Writer = "Missing data";
+            Expected.Year = "Missing data";
+            MovieInfo Actual = Service.GetMovieInfo(title);
+            Assert.AreEqual(Expected, Actual);
         }
 
         [TestMethod]
         [ExpectedException(typeof(TitleNotFoundException))]
         public void TmdbMovieInfoNotFoundExceptionTest()
         {
-            TmdbService service = TmdbService.Service;
+            TmdbService Service = TmdbService.Service;
             title = "thisIsNotAnameOfAmovie";
-            service.GetMovieInfo(title);
+            Service.GetMovieInfo(title);
         }
 
         [TestMethod]
         public void AuthenticateTest()
         {
-            TmdbService service = TmdbService.Service;
-            string expected = "OK";
-            string userName = "kogrego";
-            string password = "Gk2547963";
-            string actual = service.Authenticate(userName, password);
-            Assert.AreEqual(expected, actual);
+            TmdbService Service = TmdbService.Service;
+            string Expected = "OK";
+            string UserName = "kogrego";
+            string Password = "Gk2547963";
+            string Actual = Service.Authenticate(UserName, Password);
+            Assert.AreEqual(Expected, Actual);
         }
 
         [TestMethod]
         [ExpectedException(typeof(AuthenticationFailedException))]
         public void TmdbAuthenticationFailedExceptionTest()
         {
-            TmdbService service = TmdbService.Service;
+            TmdbService Service = TmdbService.Service;
             title = "thisIsNotAnameOfAmovie";
-            string userName = "kogrego";
-            string password = "wrongPassword";
-            service.Authenticate(userName, password);
+            string UserName = "kogrego";
+            string Password = "wrongPassword";
+            Service.Authenticate(UserName, Password);
         }
     }
 }
